3f24d85fd7c43141a11fbee8de183f4fca371013
Samihan <74051958+sivasamih@users.noreply.github.com>
2021-10-12 17:33:24 +0530

    evening updates samihan

--- sims/src/components/user/nav.js
+++ sims/src/components/user/nav.js
@@ -1,19 +1,21 @@
 import './dasboard.css';
-import { COOKIE, getCookie,deleteCookie } from "../../services/cookie";
+import { COOKIE, getCookie, deleteCookie } from "../../services/cookie";
 import * as URLS from "../../routes/constants";
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
- 
+
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 
 import Logincheck from "./logincheck";
- 
+
+
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -36,14 +38,14 @@ export default function ButtonAppBar() {
     const [isLoggedIn, setisLoggedIn] = React.useState(true);
     const [firstname, setfirstname] = React.useState("");
     const [greetings, setgreetings] = React.useState("");
-    const [branchBtnId,setbranchBtnId] = React.useState("");
-    const [token,settoken] = React.useState("");
+    const [branchBtnId, setbranchBtnId] = React.useState("");
+    const [token, settoken] = React.useState("");
 
-    
+
     useEffect(() => {
         console.log('On Load Event of UseEffect !');
 
-        if(getCookie(COOKIE.USERID)!=null){
+        if (getCookie(COOKIE.USERID) != null) {
             let token = getCookie(COOKIE.TOKEN);
             let FIRSTNAME = getCookie(COOKIE.FIRSTNAME);
             let initialName = FIRSTNAME.charAt(0).toUpperCase();
@@ -51,13 +53,13 @@ export default function ButtonAppBar() {
             let branchId = url.searchParams.get("branchId");
             let branchName = url.searchParams.get("branchName");
             let compName = url.searchParams.get("compName");
-            let branchBtnId= url.searchParams.get("branchBtnId");
+            let branchBtnId = url.searchParams.get("branchBtnId");
             console.log("===========================================");
             console.log("branchId > ", branchId);
             console.log("branchName > ", branchName);
             console.log("compName > ", compName);
             console.log("===========================================");
-    
+
             if (
                 token === "null" || compName === "null" || branchName === "null" || branchId === "null" ||
                 token == null ||
@@ -79,43 +81,50 @@ export default function ButtonAppBar() {
                 setfirstname(FIRSTNAME);
                 setbranchBtnId(branchBtnId);
                 settoken(token);
-    
+
             }
-        }else{
-            window.location.href="/loginExpired";
+        } else {
+            window.location.href = "/loginExpired";
         }
 
-        
+
 
     }, []);
 
 
-  
 
-    const closeWindow = e => { 
-        window.close();        
+
+    const closeWindow = e => {
+        window.close();
     }
 
     return (
-        <div className={classes.root}>
-        <CssBaseline />           
-            <AppBar className="navDiv" position="static">
-                <Toolbar>
-                    <Typography variant="h6" className={classes.title}>
-                        {branchName}
-                    </Typography>
-                     
+        <Fragment>
+            <div className={classes.root}>
+                <CssBaseline />
+                <AppBar className="navDiv" position="static">
+                    <Toolbar>
+                        <Typography variant="h6" className={classes.title}>
+                            {branchName}
+                        </Typography>
 
-                    <Link
-                    className="nav-exit-link"                    
-                    onClick={closeWindow}
-                    >
-                    Exit
-                    </Link>
-                                        
-                </Toolbar>
-            </AppBar>
-            <Logincheck/>
-        </div>
+
+                        <Link
+                            className="nav-exit-link"
+                            onClick={closeWindow}
+                        >
+                            Exit
+                        </Link>
+
+
+
+                    </Toolbar>
+                </AppBar>
+                <Logincheck />
+
+            </div>
+
+        </Fragment>
+
     );
 }
