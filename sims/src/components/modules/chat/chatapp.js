a4086d5971e721e3ea7cf4b35ca1748545e089b1
Samihan <74051958+sivasamih@users.noreply.github.com>
2021-10-29 17:31:15 +0530

    samihan updates

--- sims/src/components/modules/chat/chatapp.js
+++ sims/src/components/modules/chat/chatapp.js
@@ -6,7 +6,8 @@ import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
-import '../../user/dasboard.css'; 
+import Grid from "@material-ui/core/Grid";
+import '../../user/dasboard.css';
 
 
 class chatapp extends React.Component {
@@ -17,82 +18,91 @@ class chatapp extends React.Component {
     componentDidMount() { }
     render() {
 
-    const userList= (
-        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-          <ListItem alignItems="flex-start">
-            <ListItemAvatar>
-              <Avatar> A</Avatar>
-            </ListItemAvatar>
-            <ListItemText
-              primary="User 1"
-              secondary={
-                <React.Fragment>
-                  <Typography
-                    sx={{ display: 'inline' }}
-                    component="span"
-                    variant="body2"
-                    color="text.primary"
-                  >
-                    Ali Connors
-                  </Typography>
-                  {" — I'll be in your neighborhood doing errands this…"}
-                </React.Fragment>
-              }
-            />
-          </ListItem>
-          <Divider variant="inset" component="li" />
-          <ListItem alignItems="flex-start">
-            <ListItemAvatar>
-            <Avatar> B</Avatar>
-            </ListItemAvatar>
-            <ListItemText
-              primary="User 1"
-              secondary={
-                <React.Fragment>
-                  <Typography
-                    sx={{ display: 'inline' }}
-                    component="span"
-                    variant="body2"
-                    color="text.primary"
-                  >
-                    to Scott, Alex, Jennifer
-                  </Typography>
-                  {" — Wish I could come, but I'm out of town this…"}
-                </React.Fragment>
-              }
-            />
-          </ListItem>
-          <Divider variant="inset" component="li" />
-          <ListItem alignItems="flex-start">
-            <ListItemAvatar>
-            <Avatar> C</Avatar>
-            </ListItemAvatar>
-            <ListItemText
-              primary="Oui Oui"
-              secondary={
-                <React.Fragment>
-                  <Typography
-                    sx={{ display: 'inline' }}
-                    component="span"
-                    variant="body2"
-                    color="text.primary"
-                  >
-                    Sandra Adams
-                  </Typography>
-                  {' — Do you have Paris recommendations? Have you ever…'}
-                </React.Fragment>
-              }
-            />
-          </ListItem>
-        </List>
-      );
-  
+        const userList = (
+            <Fragment>
+
+                <Grid container spacing={1}>
+                    <Grid xs={12} sm={12} md={11} lg={11}>
+                        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                            <ListItem alignItems="flex-start">
+                                <ListItemAvatar>
+                                    <Avatar> A</Avatar>
+                                </ListItemAvatar>
+                                <ListItemText
+                                    primary="User 1"
+                                    secondary={
+                                        <React.Fragment>
+                                            <Typography
+                                                sx={{ display: 'inline' }}
+                                                component="span"
+                                                variant="body2"
+                                                color="text.primary"
+                                            >
+                                                Ali Connors
+                                            </Typography>
+                                            {" — I'll be in your neighborhood doing errands this…"}
+                                        </React.Fragment>
+                                    }
+                                />
+                            </ListItem>
+                            <Divider variant="inset" component="li" />
+                            <ListItem alignItems="flex-start">
+                                <ListItemAvatar>
+                                    <Avatar> B</Avatar>
+                                </ListItemAvatar>
+                                <ListItemText
+                                    primary="User 1"
+                                    secondary={
+                                        <React.Fragment>
+                                            <Typography
+                                                sx={{ display: 'inline' }}
+                                                component="span"
+                                                variant="body2"
+                                                color="text.primary"
+                                            >
+                                                to Scott, Alex, Jennifer
+                                            </Typography>
+                                            {" — Wish I could come, but I'm out of town this…"}
+                                        </React.Fragment>
+                                    }
+                                />
+                            </ListItem>
+                            <Divider variant="inset" component="li" />
+                            <ListItem alignItems="flex-start">
+                                <ListItemAvatar>
+                                    <Avatar> C</Avatar>
+                                </ListItemAvatar>
+                                <ListItemText
+                                    primary="Oui Oui"
+                                    secondary={
+                                        <React.Fragment>
+                                            <Typography
+                                                sx={{ display: 'inline' }}
+                                                component="span"
+                                                variant="body2"
+                                                color="text.primary"
+                                            >
+                                                Sandra Adams
+                                            </Typography>
+                                            {' — Do you have Paris recommendations? Have you ever…'}
+                                        </React.Fragment>
+                                    }
+                                />
+                            </ListItem>
+                        </List>
+                    </Grid>
+                </Grid>
+
+
+            </Fragment>
+        );
+
 
         return (
             <Fragment>
-             <div className="chat-app-box">
-                {userList}
-             </div>
+                <div className="chat-app-box">
+                    {userList}
+                </div>
             </Fragment>
         )
     }
